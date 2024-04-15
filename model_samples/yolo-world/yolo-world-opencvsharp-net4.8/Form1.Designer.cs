a7bdb0128bcc0e8e3396b8f37f5ce387b5099c6f
yanguojin <guojin_yjs@cumt.edu.cn>
2024-04-15 15:08:02 +0800

    Add yolo-world project, using net framework 4.8.

---
+++ model_samples/yolo-world/yolo-world-opencvsharp-net4.8/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace yolo_world_opencvsharp_net4._8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_classes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_select_model = new System.Windows.Forms.Button();
+            this.tb_model_path = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_select_input = new System.Windows.Forms.Button();
+            this.btn_infer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_input_path = new System.Windows.Forms.TextBox();
+            this.btn_load_model = new System.Windows.Forms.Button();
+            this.cb_device = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tb_msg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_classes
+            // 
+            this.tb_classes.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_classes.Location = new System.Drawing.Point(36, 128);
+            this.tb_classes.Multiline = true;
+            this.tb_classes.Name = "tb_classes";
+            this.tb_classes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_classes.Size = new System.Drawing.Size(491, 184);
+            this.tb_classes.TabIndex = 0;
+            this.tb_classes.Text = resources.GetString("tb_classes.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(32, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入要检测的类，用逗号分隔";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(549, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 500);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(522, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(561, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "YOLO-World 实时开放词汇对象检测演示";
+            // 
+            // btn_select_model
+            // 
+            this.btn_select_model.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_select_model.Location = new System.Drawing.Point(68, 513);
+            this.btn_select_model.Name = "btn_select_model";
+            this.btn_select_model.Size = new System.Drawing.Size(157, 36);
+            this.btn_select_model.TabIndex = 3;
+            this.btn_select_model.Text = "选择模型";
+            this.btn_select_model.UseVisualStyleBackColor = true;
+            this.btn_select_model.Click += new System.EventHandler(this.btn_select_model_Click);
+            // 
+            // tb_model_path
+            // 
+            this.tb_model_path.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_model_path.Location = new System.Drawing.Point(157, 354);
+            this.tb_model_path.Name = "tb_model_path";
+            this.tb_model_path.Size = new System.Drawing.Size(370, 30);
+            this.tb_model_path.TabIndex = 4;
+            this.tb_model_path.Text = "E:\\Model\\yolo-world\\yolow-l.onnx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(32, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "模型地址:";
+            // 
+            // btn_select_input
+            // 
+            this.btn_select_input.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_select_input.Location = new System.Drawing.Point(296, 513);
+            this.btn_select_input.Name = "btn_select_input";
+            this.btn_select_input.Size = new System.Drawing.Size(157, 36);
+            this.btn_select_input.TabIndex = 3;
+            this.btn_select_input.Text = "选择输入";
+            this.btn_select_input.UseVisualStyleBackColor = true;
+            this.btn_select_input.Click += new System.EventHandler(this.btn_select_input_Click);
+            // 
+            // btn_infer
+            // 
+            this.btn_infer.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_infer.Location = new System.Drawing.Point(287, 581);
+            this.btn_infer.Name = "btn_infer";
+            this.btn_infer.Size = new System.Drawing.Size(221, 48);
+            this.btn_infer.TabIndex = 3;
+            this.btn_infer.Text = "模 型 推 理";
+            this.btn_infer.UseVisualStyleBackColor = true;
+            this.btn_infer.Click += new System.EventHandler(this.btn_infer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(32, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "输入地址:";
+            // 
+            // tb_input_path
+            // 
+            this.tb_input_path.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_input_path.Location = new System.Drawing.Point(157, 397);
+            this.tb_input_path.Name = "tb_input_path";
+            this.tb_input_path.Size = new System.Drawing.Size(370, 30);
+            this.tb_input_path.TabIndex = 4;
+            this.tb_input_path.Text = "E:\\ModelData\\image\\demo_2.jpg";
+            // 
+            // btn_load_model
+            // 
+            this.btn_load_model.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_load_model.Location = new System.Drawing.Point(36, 581);
+            this.btn_load_model.Name = "btn_load_model";
+            this.btn_load_model.Size = new System.Drawing.Size(221, 48);
+            this.btn_load_model.TabIndex = 3;
+            this.btn_load_model.Text = "加 载 模 型";
+            this.btn_load_model.UseVisualStyleBackColor = true;
+            this.btn_load_model.Click += new System.EventHandler(this.btn_load_model_Click);
+            // 
+            // cb_device
+            // 
+            this.cb_device.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cb_device.FormattingEnabled = true;
+            this.cb_device.Location = new System.Drawing.Point(157, 446);
+            this.cb_device.Name = "cb_device";
+            this.cb_device.Size = new System.Drawing.Size(205, 28);
+            this.cb_device.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(32, 449);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "输入地址:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(549, 591);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1000, 500);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tb_msg
+            // 
+            this.tb_msg.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_msg.Location = new System.Drawing.Point(36, 658);
+            this.tb_msg.Multiline = true;
+            this.tb_msg.Name = "tb_msg";
+            this.tb_msg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_msg.Size = new System.Drawing.Size(490, 418);
+            this.tb_msg.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 1103);
+            this.Controls.Add(this.cb_device);
+            this.Controls.Add(this.tb_input_path);
+            this.Controls.Add(this.tb_model_path);
+            this.Controls.Add(this.btn_load_model);
+            this.Controls.Add(this.btn_infer);
+            this.Controls.Add(this.btn_select_input);
+            this.Controls.Add(this.btn_select_model);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_msg);
+            this.Controls.Add(this.tb_classes);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_classes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_select_model;
+        private System.Windows.Forms.TextBox tb_model_path;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_select_input;
+        private System.Windows.Forms.Button btn_infer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_input_path;
+        private System.Windows.Forms.Button btn_load_model;
+        private System.Windows.Forms.ComboBox cb_device;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tb_msg;
+    }
+}
+
---
+++ model_samples/yolo-world/yolo-world-opencvsharp-net4.8/Form1.cs
@@ -0,0 +1,166 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.ImgHash;
+using OpenVinoSharp;
+using OpenVinoSharp.Extensions.process;
+using OpenVinoSharp.Extensions.result;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using static OpenCvSharp.FileStorage;
+
+namespace yolo_world_opencvsharp_net4._8
+{
+    public partial class Form1 : Form
+    {
+        Core core = null;
+        Model model = null;
+        CompiledModel compiled_model = null;
+        InferRequest request = null;
+        DateTime start = DateTime.Now;
+        DateTime end = DateTime.Now;
+        List<string> classes = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            start = DateTime.Now;
+            core = new Core();
+            end = DateTime.Now;
+            tb_msg.AppendText("Initialize OpenVINO Runtime Core: " + (end - start).TotalMilliseconds + "ms.\r\n");
+
+            cb_device.Items.Add("AUTO");
+            List<string> devices = core.get_available_devices();
+            foreach (var item in devices)
+            {
+                cb_device.Items.Add(item);
+            }
+            cb_device.SelectedIndex = 0;
+        }
+
+        private void btn_select_model_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            //若要改变对话框标题
+            dlg.Title = "选择推理模型文件";
+            //设置文件过滤效果
+            dlg.Filter = "模型文件(*.pdmodel,*.onnx,*.xml)|*.pdmodel;*.onnx;*.xml";
+            //dlg.InitialDirectory = @"E:\Git_space\Csharp_deploy_Yolov8\model";
+            //判断文件对话框是否打开
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                tb_model_path.Text = dlg.FileName;
+            }
+        }
+
+        private void btn_select_input_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            //若要改变对话框标题
+            dlg.Title = "选择测试输入文件";
+            //指定当前目录
+            //dlg.InitialDirectory = System.Environment.CurrentDirectory;
+            //dlg.InitialDirectory = System.IO.Path.GetFullPath(@"..//..//..//..");
+            //设置文件过滤效果
+            dlg.Filter = "图片文件(*.png,*.jpg,*.jepg)|*.png;*.jpg;*.jepg";
+            //dlg.InitialDirectory = @"E:\Git_space\Csharp_deploy_Yolov8\demo";
+            //判断文件对话框是否打开
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                tb_input_path.Text = dlg.FileName;
+            }
+        }
+
+        private void btn_load_model_Click(object sender, EventArgs e)
+        {
+            start = DateTime.Now;
+            model = core.read_model(tb_model_path.Text);
+            end = DateTime.Now;
+            tb_msg.AppendText("Read inference model: " + (end - start).TotalMilliseconds + "ms.\r\n");
+            start = DateTime.Now;
+            compiled_model = core.compile_model(model, cb_device.SelectedItem.ToString());
+            end = DateTime.Now;
+            tb_msg.AppendText("Loading a model to the device: " + (end - start).TotalMilliseconds + "ms.\r\n");
+            start = DateTime.Now;
+            request = compiled_model.create_infer_request();
+            end = DateTime.Now;
+            tb_msg.AppendText("Create an infer request: " + (end - start).TotalMilliseconds + "ms.\r\n");
+        }
+
+        private void btn_infer_Click(object sender, EventArgs e)
+        {
+            string[] words = tb_classes.Text.Split(',');
+            classes = new List<string>(words);
+            Tensor input_tensor = request.get_input_tensor();
+            Shape input_shape = input_tensor.get_shape();
+            float factor = 0f;
+            Mat image = Cv2.ImRead(tb_input_path.Text);
+
+            pictureBox1.BackgroundImage = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+
+            start = DateTime.Now;
+            Mat mat = new Mat();
+            Cv2.CvtColor(image, mat, ColorConversionCodes.BGR2RGB);
+            mat = OpenVinoSharp.Extensions.process.Resize.letterbox_img(mat, (int)input_shape[2], out factor);
+            mat = Normalize.run(mat, true);
+            float[] input_data = Permute.run(mat);
+            input_tensor.set_data(input_data);
+            end = DateTime.Now;
+            tb_msg.AppendText("Process input images: " + (end - start).TotalMilliseconds + "ms.\r\n");
+            start = DateTime.Now;
+            request.infer();
+            end = DateTime.Now;
+            tb_msg.AppendText("Do inference synchronously: " + (end - start).TotalMilliseconds + "ms.\r\n");
+            start = DateTime.Now;
+
+            Tensor output_boxes = request.get_tensor("boxes");
+            float[] boxes = output_boxes.get_data<float>((int)output_boxes.get_size());
+            Tensor output_scores = request.get_tensor("scores");
+            float[] scores = output_scores.get_data<float>((int)output_scores.get_size());
+            Tensor output_labels = request.get_tensor("labels");
+            int[] labels = output_labels.get_data<int>((int)output_labels.get_size());
+
+            Mat result_mat = draw_bbox(image.Clone(), boxes, scores, labels, factor);
+            end = DateTime.Now;
+            tb_msg.AppendText("Process result data: " + (end - start).TotalMilliseconds + "ms.\r\n");
+            start = DateTime.Now;
+            pictureBox2.BackgroundImage = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(result_mat);
+        }
+
+        Mat draw_bbox(Mat image, float[] boxes, float[] scores, int[] labels, float factor)
+        {
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < 0.2f)
+                {
+                    continue;
+                }
+                float tx = boxes[4 * i];
+                float ty = boxes[4 * i + 1];
+                float bx = boxes[4 * i + 2];
+                float by = boxes[4 * i + 3];
+                int x = (int)(tx * factor);
+                int y = (int)(ty * factor);
+                int width = (int)((bx - tx) * factor);
+                int height = (int)((by - ty) * factor);
+                Rect box = new Rect();
+                box.X = x;
+                box.Y = y;
+                box.Width = width;
+                box.Height = height;
+                Cv2.Rectangle(image, box, new Scalar(0, 0, 255),2);
+                Cv2.PutText(image, classes[labels[i]] + "-" + scores[i].ToString("0.00"), 
+                    new OpenCvSharp.Point(x, y - 10), HersheyFonts.HersheySimplex, 1, new Scalar(255, 0, 0), 2);
+            }
+            return image;
+        }
+    }
+}
