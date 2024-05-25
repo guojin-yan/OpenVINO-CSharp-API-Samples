c238fb7efc858ba3e6ddc6da61315370661d1f1b
yanguojin <guojin_yjs@cumt.edu.cn>
2024-05-25 19:38:24 +0800

    Add static void yolov10_det_process(string model_path, string image_path, string device).

--- model_samples/yolov10/yolov10_det_emgucv/Program.cs
+++ model_samples/yolov10/yolov10_det_emgucv/Program.cs
@@ -8,6 +8,7 @@ using OpenVinoSharp.Extensions;
 using OpenVinoSharp;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using OpenVinoSharp.preprocess;
 
 namespace yolov10_det_emgucv
 {
@@ -67,7 +68,8 @@ namespace yolov10_det_emgucv
             Slog.INFO("Inference device: " + device);
             Slog.INFO("Start yolov8 model inference.");
 
-            yolov10_det(model_path, image_path, device);
+            //yolov10_det(model_path, image_path, device);
+            yolov10_det_process(model_path, image_path, device);
         }
 
         static void yolov10_det(string model_path, string image_path, string device)
@@ -112,6 +114,129 @@ namespace yolov10_det_emgucv
             //Marshal.Copy(input_mat.Ptr, input_data, 0, input_data.Length);
             input_mat.CopyTo<float>(input_data);
             input_tensor.set_data<float>(input_data);
+
+            end = DateTime.Now;
+            Slog.INFO("6. Set up input data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 7. Do inference synchronously --------
+            infer_request.infer();
+            start = DateTime.Now;
+            infer_request.infer();
+            end = DateTime.Now;
+            Slog.INFO("7. Do inference synchronously success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 8. Get infer result data --------
+            start = DateTime.Now;
+            Tensor output_tensor = infer_request.get_output_tensor();
+            int output_length = (int)output_tensor.get_size();
+            float[] output_data = output_tensor.get_data<float>(output_length);
+            end = DateTime.Now;
+            Slog.INFO("8. Get infer result data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+
+            // -------- Step 9. Process reault  --------
+            start = DateTime.Now;
+            // Storage results list
+            List<Rectangle> position_boxes = new List<Rectangle>();
+            List<int> class_ids = new List<int>();
+            List<float> confidences = new List<float>();
+
+            // Preprocessing output results
+            for (int i = 0; i < output_data.Length / 6; i++)
+            {
+                int s = 6 * i;
+                if ((float)output_data[s + 4] > 0.5)
+                {
+                    float cx = output_data[s + 0];
+                    float cy = output_data[s + 1];
+                    float dx = output_data[s + 2];
+                    float dy = output_data[s + 3];
+                    int x = (int)((cx) * factor);
+                    int y = (int)((cy) * factor);
+                    int width = (int)((dx - cx) * factor);
+                    int height = (int)((dy - cy) * factor);
+                    Rectangle box = new Rectangle();
+                    box.X = x;
+                    box.Y = y;
+                    box.Width = width;
+                    box.Height = height;
+
+                    position_boxes.Add(box);
+                    class_ids.Add((int)output_data[s + 5]);
+                    confidences.Add((float)output_data[s + 4]);
+                }
+            }
+            end = DateTime.Now;
+            Slog.INFO("9. Process reault  success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            for (int i = 0; i < class_ids.Count; i++)
+            {
+                int index = i;
+                CvInvoke.Rectangle(image, position_boxes[index], new MCvScalar(0, 0, 255), 2, LineType.Filled);
+                CvInvoke.Rectangle(image, new Rectangle(new Point(position_boxes[index].X, position_boxes[index].Y),
+                 new Size(position_boxes[index].Width, 30)), new MCvScalar(0, 255, 255), -1);
+                CvInvoke.PutText(image, CocoOption.lables[class_ids[index]] + "-" + confidences[index].ToString("0.00"),
+                    new Point(position_boxes[index].X, position_boxes[index].Y + 25),
+                    FontFace.HersheySimplex, 0.8, new MCvScalar(0, 0, 0), 2);
+            }
+            string output_path = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(image_path)),
+                Path.GetFileNameWithoutExtension(image_path) + "_result.jpg");
+            CvInvoke.Imwrite(output_path, image);
+            Slog.INFO("The result save to " + output_path);
+            CvInvoke.Imshow("Result", image);
+            CvInvoke.WaitKey(0);
+        }
+
+        static void yolov10_det_process(string model_path, string image_path, string device)
+        {
+            DateTime start = DateTime.Now;
+            // -------- Step 1. Initialize OpenVINO Runtime Core --------
+            Core core = new Core();
+            DateTime end = DateTime.Now;
+            Slog.INFO("1. Initialize OpenVINO Runtime Core success, time spend: " + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 2. Read inference model --------
+            start = DateTime.Now;
+            OpenVinoSharp.Model model = core.read_model(model_path);
+            end = DateTime.Now;
+            Slog.INFO("2. Read inference model success, time spend: " + (end - start).TotalMilliseconds + "ms.");
+            OvExtensions.printf_model_info(model);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            Tensor input_tensor_pro = new Tensor(new OvType(ElementType.U8), new Shape(1, 640, 640, 3));
+            InputInfo input_info = processor.input(0);
+            InputTensorInfo input_tensor_info = input_info.tensor();
+            input_tensor_info.set_from(input_tensor_pro).set_layout(new Layout("NHWC")).set_color_format(ColorFormat.BGR);
+
+            PreProcessSteps process_steps = input_info.preprocess();
+            process_steps.convert_color(ColorFormat.RGB).resize(ResizeAlgorithm.RESIZE_LINEAR)
+                .convert_element_type(new OvType(ElementType.F32)).scale(255.0f).convert_layout(new Layout("NCHW"));
+            OpenVinoSharp.Model new_model = processor.build();
+            // -------- Step 3. Loading a model to the device --------
+            start = DateTime.Now;
+            CompiledModel compiled_model = core.compile_model(new_model, device);
+            end = DateTime.Now;
+            Slog.INFO("3. Loading a model to the device success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 4. Create an infer request --------
+            start = DateTime.Now;
+            InferRequest infer_request = compiled_model.create_infer_request();
+            end = DateTime.Now;
+            Slog.INFO("4. Create an infer request success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 5. Process input images --------
+            start = DateTime.Now;
+            Mat image = new Mat(image_path); // Read image by opencvsharp
+            int max_image_length = image.Cols > image.Rows ? image.Cols : image.Rows;
+            Mat max_image = Mat.Zeros(max_image_length, max_image_length, DepthType.Cv8U, 3);
+            Rectangle roi = new Rectangle(0, 0, image.Cols, image.Rows);
+            image.CopyTo(new Mat(max_image, roi));
+            CvInvoke.Resize(max_image, max_image, new Size(640, 640));
+            float factor = (float)(max_image_length / 640.0);
+            end = DateTime.Now;
+            Slog.INFO("5. Process input images success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 6. Set up input data --------
+            start = DateTime.Now;
+            Tensor input_tensor = infer_request.get_input_tensor();
+            Shape input_shape = input_tensor.get_shape();
+            byte[] input_data = new byte[input_shape[1] * input_shape[2] * input_shape[3]];
+            //max_image.GetArray<int>(out input_data);
+            max_image.CopyTo<byte>(input_data);
+            IntPtr destination = input_tensor.data();
+            Marshal.Copy(input_data, 0, destination, input_data.Length);
+            //input_tensor.set_data<byte>(input_data);
 
             end = DateTime.Now;
             Slog.INFO("6. Set up input data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
--- model_samples/yolov10/yolov10_det_opencvsharp/Program.cs
+++ model_samples/yolov10/yolov10_det_opencvsharp/Program.cs
@@ -6,6 +6,7 @@ using OpenVinoSharp.Extensions.utility;
 using OpenVinoSharp.Extensions;
 using OpenCvSharp;
 using OpenCvSharp.Dnn;
+using OpenVinoSharp.preprocess;
 
 namespace yolov10_det_opencvsharp
 {
@@ -65,7 +66,8 @@ namespace yolov10_det_opencvsharp
             Slog.INFO("Inference device: " + device);
             Slog.INFO("Start yolov8 model inference.");
 
-            yolov10_det(model_path, image_path, device);
+            //yolov10_det(model_path, image_path, device);
+            yolov10_det_process(model_path, "E:\\ModelData\\image\\bus.jpg", device);
         }
         static void yolov10_det(string model_path, string image_path, string device)
         {
@@ -108,6 +110,129 @@ namespace yolov10_det_opencvsharp
             float[] input_data = new float[input_shape[1] * input_shape[2] * input_shape[3]];
             Marshal.Copy(input_mat.Ptr(0), input_data, 0, input_data.Length);
             input_tensor.set_data<float>(input_data);
+
+            end = DateTime.Now;
+            Slog.INFO("6. Set up input data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 7. Do inference synchronously --------
+            infer_request.infer();
+            start = DateTime.Now;
+            infer_request.infer();
+            end = DateTime.Now;
+            Slog.INFO("7. Do inference synchronously success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 8. Get infer result data --------
+            start = DateTime.Now;
+            Tensor output_tensor = infer_request.get_output_tensor();
+            int output_length = (int)output_tensor.get_size();
+            float[] output_data = output_tensor.get_data<float>(output_length);
+            end = DateTime.Now;
+            Slog.INFO("8. Get infer result data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+
+            //// -------- Step 9. Process reault  --------
+            start = DateTime.Now;
+
+            List<Rect> position_boxes = new List<Rect>();
+            List<int> class_ids = new List<int>();
+            List<float> confidences = new List<float>();
+
+            // Preprocessing output results
+            for (int i = 0; i < output_data.Length / 6; i++)
+            {
+                int s = 6 * i;
+                if ((float)output_data[s + 4] > 0.5)
+                {
+                    float cx = output_data[s + 0];
+                    float cy = output_data[s + 1];
+                    float dx = output_data[s + 2];
+                    float dy = output_data[s + 3];
+                    int x = (int)((cx) * factor);
+                    int y = (int)((cy) * factor);
+                    int width = (int)((dx - cx) * factor);
+                    int height = (int)((dy - cy) * factor);
+                    Rect box = new Rect();
+                    box.X = x;
+                    box.Y = y;
+                    box.Width = width;
+                    box.Height = height;
+
+                    position_boxes.Add(box);
+                    class_ids.Add((int)output_data[s + 5]);
+                    confidences.Add((float)output_data[s + 4]);
+                }
+            }
+
+
+            end = DateTime.Now;
+            Slog.INFO("9. Process reault  success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            for (int i = 0; i < class_ids.Count; i++)
+            {
+                int index = i;
+                Cv2.Rectangle(image, position_boxes[index], new Scalar(0, 0, 255), 2, LineTypes.Link8);
+                Cv2.Rectangle(image, new OpenCvSharp.Point(position_boxes[index].TopLeft.X, position_boxes[index].TopLeft.Y + 30),
+                    new OpenCvSharp.Point(position_boxes[index].BottomRight.X, position_boxes[index].TopLeft.Y), new Scalar(0, 255, 255), -1);
+                Cv2.PutText(image, class_ids[index] + "-" + confidences[index].ToString("0.00"),
+                    new OpenCvSharp.Point(position_boxes[index].X, position_boxes[index].Y + 25),
+                    HersheyFonts.HersheySimplex, 0.8, new Scalar(0, 0, 0), 2);
+            }
+            string output_path = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(image_path)),
+                Path.GetFileNameWithoutExtension(image_path) + "_result.jpg");
+            Cv2.ImWrite(output_path, image);
+            Slog.INFO("The result save to " + output_path);
+            Cv2.ImShow("Result", image);
+            Cv2.WaitKey(0);
+        }
+        static void yolov10_det_process(string model_path, string image_path, string device)
+        {
+            DateTime start = DateTime.Now;
+            // -------- Step 1. Initialize OpenVINO Runtime Core --------
+            Core core = new Core();
+            DateTime end = DateTime.Now;
+            Slog.INFO("1. Initialize OpenVINO Runtime Core success, time spend: " + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 2. Read inference model --------
+            start = DateTime.Now;
+            Model model = core.read_model(model_path);
+            end = DateTime.Now;
+            Slog.INFO("2. Read inference model success, time spend: " + (end - start).TotalMilliseconds + "ms.");
+            OvExtensions.printf_model_info(model);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            Tensor input_tensor_pro = new Tensor(new OvType(ElementType.U8), new Shape(1, 640, 640, 3));
+            InputInfo input_info = processor.input(0);
+            InputTensorInfo input_tensor_info = input_info.tensor();
+            input_tensor_info.set_from(input_tensor_pro).set_layout(new Layout("NHWC")).set_color_format(ColorFormat.BGR);
+
+            PreProcessSteps process_steps = input_info.preprocess();
+            process_steps.convert_color(ColorFormat.RGB).resize(ResizeAlgorithm.RESIZE_LINEAR)
+                .convert_element_type(new OvType(ElementType.F32)).scale(255.0f).convert_layout(new Layout("NCHW"));
+            Model new_model = processor.build();
+            // -------- Step 3. Loading a model to the device --------
+            start = DateTime.Now;
+            CompiledModel compiled_model = core.compile_model(new_model, device);
+            end = DateTime.Now;
+            Slog.INFO("3. Loading a model to the device success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 4. Create an infer request --------
+            start = DateTime.Now;
+            InferRequest infer_request = compiled_model.create_infer_request();
+            end = DateTime.Now;
+            Slog.INFO("4. Create an infer request success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 5. Process input images --------
+            start = DateTime.Now;
+            Mat image = new Mat(image_path); // Read image by opencvsharp
+            int max_image_length = image.Cols > image.Rows ? image.Cols : image.Rows;
+            Mat max_image = Mat.Zeros(new OpenCvSharp.Size(max_image_length, max_image_length), MatType.CV_8UC3);
+            Rect roi = new Rect(0, 0, image.Cols, image.Rows);
+            image.CopyTo(new Mat(max_image, roi));
+            Cv2.Resize(max_image, max_image, new OpenCvSharp.Size(640, 640));
+            float factor = (float)(max_image_length / 640.0);
+            end = DateTime.Now;
+            Slog.INFO("5. Process input images success, time spend:" + (end - start).TotalMilliseconds + "ms.");
+            // -------- Step 6. Set up input data --------
+            start = DateTime.Now;
+            Tensor input_tensor = infer_request.get_input_tensor();
+            Shape input_shape = input_tensor.get_shape();
+            byte[] input_data = new byte[input_shape[1] * input_shape[2] * input_shape[3]];
+            //max_image.GetArray<int>(out input_data);
+            Marshal.Copy(max_image.Ptr(0), input_data, 0, input_data.Length);
+            IntPtr destination = input_tensor.data();
+            Marshal.Copy(input_data, 0, destination, input_data.Length);
 
             end = DateTime.Now;
             Slog.INFO("6. Set up input data success, time spend:" + (end - start).TotalMilliseconds + "ms.");
